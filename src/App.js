1af583e16cb72e565ecdae756ffdc13cd3bdaa3d
rahad2694 <rahad.haw@gmail.com>
2022-04-17 02:04:34 +0600

    Nav bar finalized

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
 import { Toaster } from 'react-hot-toast';
+import { Routes } from 'react-router-dom';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
+      <Routes>
+
+      </Routes>
       <Toaster />
     </div>
   );
