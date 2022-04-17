563b4b09b60541d8b5094d8f5ead1ed632db0b23
rahad2694 <rahad.haw@gmail.com>
2022-04-17 15:48:54 +0600

    Login Page Fully Functional

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { signOut } from 'firebase/auth';
 import { Link, NavLink } from 'react-router-dom';
+import auth from '../../Firebase/Firebase.init';
 import logo from '../../images/logo.png'
 
 const Header = () => {
+    const [user] = useAuthState(auth);
     return (
         <nav className='grid grid-cols-2 py-1 sticky top-0 bg-white z-50'>
             <div className='flex ml-2 md:ml-10'>
@@ -14,7 +18,10 @@ const Header = () => {
             <div className='flex justify-evenly align-middle mt-6 sm:mt-4 md:mx-20'>
                 <NavLink className={({ isActive }) => (isActive) ? 'text-red-600' : ''} to='/blogs'>Blogs</NavLink>
                 <NavLink className={({ isActive }) => (isActive) ? 'text-red-600' : ''} to='/about'>About</NavLink>
-                <NavLink className={({ isActive }) => (isActive) ? 'text-red-600' : ''} to='/login'>Login</NavLink>
+                {user ? <button onClick={()=>signOut(auth)} className='text-blue-600 hover:text-orange-500 rounded-xl mb-6 sm:mb-4'>Logout</button>
+                    :
+                    <NavLink className={({ isActive }) => (isActive) ? 'text-red-600' : ''} to='/login'>Login</NavLink>
+                }
             </div>
         </nav>
     );
