d8188d7a7a303451190f82fb0f0e93a212e5f314
rahad2694 <rahad.haw@gmail.com>
2022-04-18 17:14:15 +0600

    Stepper added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Carousel from '../Carousel/Carousel';
 import Services from '../Services/Services';
+import Stepper from '../Stepper/Stepper';
+import 'tw-elements';
 
 const Home = () => {
     return (
         <div>
             <Carousel></Carousel>
             <Services></Services>
+            <Stepper></Stepper>
         </div>
     );
 };
---
+++ src/components/Stepper/Stepper.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const Stepper = () => {
+    return (
+        <ul className="stepper" data-mdb-stepper="stepper">
+            <li className="stepper-step stepper-active">
+                <div className="stepper-head">
+                    <span className="stepper-head-icon"> 1 </span>
+                    <span className="stepper-head-text"> step1 </span>
+                </div>
+                <div className="stepper-content">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+                    magna aliqua.
+                </div>
+            </li>
+            <li className="stepper-step">
+                <div className="stepper-head">
+                    <span className="stepper-head-icon"> 2 </span>
+                    <span className="stepper-head-text"> step2 </span>
+                </div>
+                <div className="stepper-content">
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </div>
+            </li>
+            <li className="stepper-step">
+                <div className="stepper-head">
+                    <span className="stepper-head-icon"> 3 </span>
+                    <span className="stepper-head-text"> step3 </span>
+                </div>
+                <div className="stepper-content">
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                </div>
+            </li>
+        </ul>
+    );
+};
+
+export default Stepper;
